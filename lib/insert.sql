3a3def3b594d4dc74232a21acb9d133652729dc3
Megan Comeau <mlcomeau99@gmail.com>
2020-05-12 20:08:14 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Booboo", 2, "male", "brown", "friendly", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fuzzy", 3, "male", "black", "quiet", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mama Bear", 4, "female", "black", "goofy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Louisa", 2, "female", "brown", "friendly", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Margret", 1, "female", "black", "quiet", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Papa Bear", 6, "male", "brown", "unfriendly", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "male", "black", "quiet", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bopeep", 1, "female", "brown", "goofy", true);
