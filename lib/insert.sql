0c6c4b025669d06ecc51019a1d27aca2c3e93e1b
Megan Comeau <mlcomeau99@gmail.com>
2020-05-12 20:09:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Booboo", 2, "male", "brown", "friendly", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fuzzy", 3, "male", "black", "quiet", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mama Bear", 4, "female", "black", "goofy", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Louisa", 2, "female", "brown", "friendly", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Margret", 1, "female", "black", "quiet", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Papa Bear", 6, "male", "brown", "unfriendly", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "male", "black", "quiet", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bopeep", 1, "female", "brown", "goofy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Booboo", 2, "male", "brown", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fuzzy", 3, "male", "black", "quiet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mama Bear", 4, "female", "black", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Louisa", 2, "female", "brown", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Margret", 1, "female", "black", "quiet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Papa Bear", 6, "male", "brown", "unfriendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "male", "black", "quiet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bopeep", 1, "female", "brown", "goofy", 1);
